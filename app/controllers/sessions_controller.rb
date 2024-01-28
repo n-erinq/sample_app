219886ea72f3a5c9d5e0513386e0b8c028bcfd32
n-erinq <138581832+n-erinq@users.noreply.github.com>
2024-01-28 02:31:08 +0000

    フラッシュのテスト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
