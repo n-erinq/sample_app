87aa8c821afebd9ca44e9e59c55d21c881e8370e
n-erinq <138581832+n-erinq@users.noreply.github.com>
2024-02-10 07:38:02 +0000

    記憶トークンと暗号化1

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,5 +14,9 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
 
