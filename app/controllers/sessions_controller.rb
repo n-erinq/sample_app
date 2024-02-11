452d3ed97bb24b504e9432bb1145739246204ceb
n-erinq <138581832+n-erinq@users.noreply.github.com>
2024-02-11 22:58:35 +0000

    ログイン状態の保持

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
        reset_session      # ログインの直前に必ずこれを書くこと
+       remember user
        log_in user
        redirect_to user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  # ランダムなトークンを返す
   def User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -24,6 +25,11 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
 
