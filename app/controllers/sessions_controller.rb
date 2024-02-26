6205ce38ae1669ec7ee97ffaf0349adab58dce72
n-erinq <138581832+n-erinq@users.noreply.github.com>
2024-02-26 12:18:20 +0000

    フレンドリーフォワーディング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+       forwarding_url = session[:forwarding_url]
        reset_session      # ログインの直前に必ずこれを書くこと
        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
        log_in user
-       redirect_to user
+       redirect_to forwarding_url || user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new', status: :unprocessable_entity
